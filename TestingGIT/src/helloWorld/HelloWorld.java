a0a41bb831692fd9986d315b554365fbac348012
jesiryan <jesiryan@gmail.com>
2014-07-10 14:17:01 +0100

    initial push from second

--- TestingGIT/src/helloWorld/HelloWorld.java
+++ TestingGIT/src/helloWorld/HelloWorld.java
@@ -12,5 +12,10 @@
 package helloWorld;
 
 public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
 
 }
