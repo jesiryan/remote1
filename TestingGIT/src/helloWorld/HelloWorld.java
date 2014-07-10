81c354307b93aa4525ce41219a238819920f2878
jesiryan <jesiryan@gmail.com>
2014-07-10 14:20:42 +0100

    hi

--- TestingGIT/src/helloWorld/HelloWorld.java
+++ TestingGIT/src/helloWorld/HelloWorld.java
@@ -14,8 +14,8 @@ package helloWorld;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("What?");
 	}
 
 }
